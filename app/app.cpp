421dfa2add66d960c488cdcb69a138dc6f80b296
Marko Šeparović <maseparov@vsite.hr>
2024-10-30 11:28:25 +0100

    stavio app.cpp i app.h u staged

--- app/app.cpp
+++ app/app.cpp
@@ -1,49 +1,34 @@
 #include "app.h"
-#include <sstream>
-#include <iomanip>
 #include <format>
 
 namespace vsite::oop::v1
 {
     std::string to_hex(int value) {
-        std::stringstream ss;
-        ss << std::uppercase << std::hex << value;
-        return ss.str();
+        return std::format("{:X}", value); // koristi {:X} za formatiranje u heksadecimalni format sa velikim slovima
     }
-    
+
     std::string to_exp(double value) {
-        std::stringstream ss;
-        ss << std::scientific << std::setprecision(2) << value;
-        return ss.str();
+        return std::format("{:.2e}", value); // koristi {:.2e} za formatiranje u eksponencijalni format sa dvije decimale
     }
 
-	void mult_table(const int num, std::ostream& ss)
-	{
-		if (num == 0 || num > 20)
-		{
-			return;
-		}
+    void mult_table(const int num, std::ostream& ss) {
+        if (num == 0 || num > 20) {
+            return;
+        }
 
-		for (int i = 0; i <= num; ++i)
-		{
-			for (int j = 0; j <= num; ++j)
-			{
-				if (j == 0)
-				{
-					ss << std::format("{}", i);
-
-				}
-				else if (i == 0)
-				{
-					ss << std::format("{:4}", j);
-				}
-				else
-				{
-					ss << std::format("{:4}", i * j);
-				}
-
-			}
-			ss << "\n";
-		}
-	}
+        for (int i = 0; i <= num; ++i) {
+            for (int j = 0; j <= num; ++j) {
+                if (j == 0) {
+                    ss << std::format("{}", i);
+                }
+                else if (i == 0) {
+                    ss << std::format("{:4}", j);
+                }
+                else {
+                    ss << std::format("{:4}", i * j);
+                }
+            }
+            ss << "\n";
+        }
+    }
 }
--- app/app.h
+++ app/app.h
@@ -6,7 +6,7 @@
 namespace vsite::oop::v1
 {
     std::string to_hex(int value);
-
+    
     std::string to_exp(double value);
 
     void mult_table(int n, std::ostream& out);
